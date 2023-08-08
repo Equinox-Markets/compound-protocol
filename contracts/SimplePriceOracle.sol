371b4eafed12fce22f3ae9a8c27ca4debecd11e7
Ethan <sovernfinance@gmail.com>
2023-08-08 12:48:37 -0500

    updates

--- contracts/SimplePriceOracle.sol
+++ contracts/SimplePriceOracle.sol
@@ -1,56 +1,42 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.10;
 
-import "./PriceOracle.sol";
-import "./CToken.sol";
+import "./CErc20.sol";
+import "./IERC20.sol";
+import "./SafeMath.sol";
 
-contract SimplePriceOracle is PriceOracle {
-  mapping(address => uint) prices;
+interface ChainLinkPriceOracle {
+    function latestAnswer() external view returns (uint256);
+    function decimals() external view returns (uint8);
+}
 
-  // Chainlink oracle approval mapping
-  mapping(address => bool) public approvedOracles;
+contract EquinoxPriceOracle {
+  using SafeMath for uint256;
+  mapping(bytes32 => address) public Oracles;
 
-  event PricePosted(address asset, uint previousPriceMantissa, uint requestedPriceMantissa, uint newPriceMantissa);
+ constructor() {
+    // assign the oracle for underlyingPrice to the symbol for each market
+    Oracles[stringToBytes("eFTM")] = 0xf4766552D15AE4d256Ad41B6cf2933482B0680dc;
+    Oracles[stringToBytes("eBTC")] = 0x8e94C22142F4A64b99022ccDd994f4e9EC86E4B4;
+    Oracles[stringToBytes("eETH")] = 0x11DdD3d147E5b83D01cee7070027092397d63658;
+    Oracles[stringToBytes("eUSDC")] = 0x2553f4eeb82d5A26427b8d1106C51499CBa5D99c;
 
-  modifier onlyApprovedOracle() {
-    require(approvedOracles[msg.sender], "Not approved oracle");
-    _;
   }
 
-  function getUnderlyingPrice(CToken cToken) public view returns (uint) {
-    address asset = _getUnderlyingAddress(cToken);
-    if (approvedOracles[msg.sender]) {
-      return prices[asset];
-    } else {
-      return 0;
+  function stringToBytes (string memory s) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(s));
+  }
+
+function getUnderlyingDecimals(CToken ctoken) public view returns (uint) {
+    address underlying = CErc20(address(ctoken)).underlying();
+    return IERC20(underlying).decimals();
+}
+
+    function getUnderlyingPrice(CToken ctoken) public view returns (uint) {
+        bytes32 key = stringToBytes(ctoken.symbol());
+        ChainLinkPriceOracle oracle = ChainLinkPriceOracle(Oracles[key]);
+        // Scale to USD value with 18 decimals
+        return oracle.latestAnswer().mul(10**(28 - getUnderlyingDecimals(ctoken)));
     }
-  }
-
-  function setUnderlyingPrice(CToken cToken, uint underlyingPriceMantissa) public onlyApprovedOracle {
-    address asset = _getUnderlyingAddress(cToken);
-    emit PricePosted(asset, prices[asset], underlyingPriceMantissa, underlyingPriceMantissa);
-    prices[asset] = underlyingPriceMantissa;
-  }
-
-  // Approve Chainlink oracle
-  function approvePriceOracle(address oracle) public {
-    require(oracle != address(0), "Cannot approve zero address");
-    approvedOracles[oracle] = true;
-  }
-
-  function setChainlinkPriceOracle() public {
-    address chainlinkOracle = 0x150A58e9E6BF69ccEb1DBA5ae97C166DC8792539;
-    approvePriceOracle(chainlinkOracle);
-  }
-
-  function setDirectPrice(address asset, uint price) public {
-    emit PricePosted(asset, prices[asset], price, price);
-    prices[asset] = price;
-  }
-
-  // v1 price oracle interface for use as backing of proxy
-  function assetPrices(address asset) external view returns (uint) {
-    return prices[asset];
-  }
 }
 
